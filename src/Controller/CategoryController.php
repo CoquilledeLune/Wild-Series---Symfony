28f7068e286cfccb9a6946ebf897d1da1bf4eb31
CoquilledeLune <ljacobieski@gmail.com>
2023-01-01 17:51:15 +0100

    possibility to add new categories and new programs

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,10 +4,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,14 +18,32 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 #[Route('/category', name: 'category_')]
 class CategoryController extends AbstractController
 {
-  #[Route('/', name: 'navbar')]
+  #[Route('/', name: 'index')]
   public function index(CategoryRepository $categoryRepository): Response
     {
       $categories = $categoryRepository->findAll();
 
-      return $this->render('program/_navbar.html.twig', 
+      return $this->render('base.html.twig', 
         ['categories' => $categories]
       );
+    }
+
+  #[Route('/new', name:'new')]
+  public function new(Request $request, CategoryRepository $categoryRepository): Response
+    {
+      $category = new Category(); 
+      $form = $this->createForm(CategoryType::class, $category); 
+      // Get data from HTTP request
+      $form->handleRequest($request);
+      // Was the form submitted ?
+      if ($form->isSubmitted()) {
+        $categoryRepository->save($category, true); 
+        // Redirect to categories list
+        return $this->redirectToRoute('category_index');
+      }
+      return $this->renderForm('category/new.html.twig', [
+        'form' => $form,
+      ]);
     }
 /** Déclarer une route avec une variable, la variable se met entre accolades dans requirements on lui dit 
  * de quel type elle doit être, puis la méthode ('GET') et le name c'est le nom de ma route */
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,11 +7,12 @@ namespace App\Controller;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramType;
 use App\Repository\EpisodeRepository;
 use App\Repository\ProgramRepository;
 use App\Repository\SeasonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,6 +28,24 @@ class ProgramController extends AbstractController
       return $this->render('program/index.html.twig', 
         ['programs' => $programs]
       );
+    }
+
+    #[Route('/new', name:'new')]
+  public function new(Request $request, ProgramRepository $programRepository): Response
+    {
+      $program = new Program(); 
+      $form = $this->createForm(ProgramType::class, $program); 
+      // Get data from HTTP request
+      $form->handleRequest($request);
+      // Was the form submitted ?
+      if ($form->isSubmitted()) {
+        $programRepository->save($program, true); 
+        // Redirect to categories list
+        return $this->redirectToRoute('program_index');
+      }
+      return $this->renderForm('program/new.html.twig', [
+        'form' => $form,
+      ]);
     }
 
     #[Route('/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'show')]
